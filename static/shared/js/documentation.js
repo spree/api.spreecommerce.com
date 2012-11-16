c9f9b31d55c3ddb83fa56449a61a37704dc37888
Alexey Topolyanskiy <devilcoders@gmail.com>
2012-11-16 18:57:15 +0200

    [WIP] Some navigation fixes

--- static/shared/js/documentation.js
+++ static/shared/js/documentation.js
@@ -1,84 +1,41 @@
-$(document).ready(function(){
-  $("#content").find('h1, h2, h3').each(function(){
-    $(this).prepend("<a href='#"+$(this).attr("id")+"'><i clas='icon-link'><i> ")
-  })
-})
-
 // Init sidebar
 $(function() {
-  var activeItem,
-      helpList = $('#js-sidebar .js-topic'),
-      firstOccurance = true
 
-  // hide list items at startup
-  if($('body.api') && window.location){
-    var reg = /\/\/[^\/]+(\/.+)/g,
-        docUrl = reg.exec(window.location.toString())
-    if(docUrl){
-      $('#js-sidebar .js-topic a').each(function(){
-        var url = $(this).attr('href').toString()
-        if(url.indexOf(docUrl[1]) >= 0 && url.length == docUrl[1].length){
-          $(this).parent('li').addClass('disable')
-          var parentTopic = $(this).parentsUntil('div.sidebar-module > ul').last()
-          parentTopic.addClass('js-current')
-          parentTopic.find('.js-expand-btn').toggleClass('collapsed expanded')
-        }
-      })
+  // Add anchor links to headers
+  $("#content").find('h1, h2, h3').each(function(){
+    $(this).prepend("<a href='#"+$(this).attr("id")+"'><i clas='icon-link'><i> ")
+  });
+
+  // Sidebar menu
+  var sidebar_menu = $("#sidebar-menu")
+  
+  sidebar_menu.find('h3 a.js-expand-btn').click(function(e){
+    e.preventDefault();
+    var icon = $(this).find('i');
+
+    if(icon.attr('class') == 'icon-right-open'){
+      icon.removeClass('icon-right-open').addClass('icon-down-open');
+      sidebar_menu.find('a.active').removeClass('active');
+      icon.parent().next().addClass('active');
+      icon.parent().parent().next().stop().slideDown();
     }
+    else{
+      icon.removeClass('icon-down-open').addClass('icon-right-open');  
+      sidebar_menu.find('a.active').removeClass('active');
+      icon.parent().parent().next().stop().slideUp();
+    }
+  });
+
+  var current_url = window.location.pathname
+
+  var active_menu = sidebar_menu.find('a[href="'+current_url+'"]')
+  active_menu.addClass('active');  
+  if(active_menu.parent().next().attr('class') == 'js-guides'){
+    active_menu.parent().next().show();
+    active_menu.prev().find('i.icon-right-open').removeClass('icon-right-open').addClass('icon-down-open')
   }
-
-  $('#js-sidebar .js-topic').each(function(){
-    if(($(this).find('.disable').length == 0 || firstOccurance == false) &&
-    $(this).hasClass('js-current') != true){
-      $(this).find('.js-guides').children().hide()
-    } else {
-      activeItem = $(this).index()
-      firstOccurance = false
-    }
-  })
-
-  // Toggle style list. Expanded items stay
-  // expanded when new items are clicked.
-  $('#js-sidebar .js-toggle-list .js-expand-btn').click(function(){
-    var clickedTopic = $(this).parents('.js-topic'),
-        topicGuides  = clickedTopic.find('.js-guides li')
-    $(this).toggleClass('collapsed expanded')
-    topicGuides.toggle(100)
-    return false
-  })
-
-  // Accordion style list. Expanded items
-  // collapse when new items are clicked.
-  $('#js-sidebar .js-accordion-list .js-topic h3 a').click(function(){
-    var clickedTopic = $(this).parents('.js-topic'),
-        topicGuides = clickedTopic.find('.js-guides li')
-
-    if(activeItem != clickedTopic.index()){
-      if(helpList.eq(activeItem)){
-        helpList.eq(activeItem).find('.js-guides li').toggle(100)
-      }
-      activeItem = clickedTopic.index()
-      topicGuides.toggle(100)
-    } else {
-      activeItem = undefined
-      topicGuides.toggle(100)
-    }
-
-    return false
-  })
-
-  $('.help-search .search-box').focus(function(){
-    $(this).css('background-position','0px -25px')
-  })
-
-  $('.help-search .search-box').focusout(function(){
-    if($(this).val() == ''){
-      $(this).css('background-position','0px 0px')
-    }
-  })
-
-  // Dynamic year for footer copyright
-  var currentYear = (new Date).getFullYear();
-  $("#year").text( (new Date).getFullYear() );
-
+  else {
+    active_menu.parent().parent().show()
+  }
+  
 });
