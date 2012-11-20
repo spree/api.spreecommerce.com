8ee66ad22e2cade08ae63050ba44163eef0edd59
Alexey Topolyanskiy <devilcoders@gmail.com>
2012-11-20 17:44:16 +0200

    Add github block on front page

---
+++ static/shared/js/github.js
@@ -0,0 +1,21 @@
+$(function(){
+  $.ajax({
+    url: "https://api.github.com/repos/spree/spree/commits?path=api&until=", 
+    success: function(data){
+      $.each(data, function(index, item){
+        if(index < 5) {          
+          $("#commits").append(
+                               "<li class='commit'><i class='icon-dot'></i>" +
+                                "<a href='https://github.com/spree/spree/commit/" + item.sha + "' target='_blank'>" + item.commit.message + "</a>" + 
+                                "<div class='info'>"+
+                                  "<span class='commit-date'>" + item.commit.committer.date + "</span>" +
+                                  "<span class='commit-author'><a href='https://github.com/" + item.author.login + "'>@" + item.author.login + "</a></span>" +
+                                "</div>" +
+                               "</li>"
+                               )
+        }
+      });
+    }
+  });
+
+});
