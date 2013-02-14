5eb1a4561c79ea1d744083fcb010fa56140cd88f
Ryan Bigg <radarlistener@gmail.com>
2013-02-14 11:56:58 +1100

    Process h3 subheadings

--- lib/filters/add_toc.rb
+++ lib/filters/add_toc.rb
@@ -6,14 +6,30 @@ class AddTOCFilter < Nanoc::Filter
     content.gsub('{{TOC}}') do
       # Find all top-level sections
       doc = Nokogiri::HTML(content)
-      headers = doc.xpath('//h2').map do |header|
-        { :title => header.inner_html, :id => header['id'] }
+      headers = []
+      doc.css("#main-content").css("h2, h3").each do |header_tag|
+        header = { :title => header_tag.inner_html, :id => header_tag['id'] }
+        if header_tag.name == 'h2'
+          headers << header
+          @current_header = header
+        else
+          @current_header[:subs] ||= []
+          @current_header[:subs] << { :title => header_tag.inner_html, :id => header_tag['id'] }
+        end
       end
 
       # Build table of contents
       res = '<ol class="toc">'
       headers.each do |header|
-        res << %[<li><a href="##{header[:id]}">#{header[:title]}</a></li>]
+        res << %[<li><a href="##{header[:id]}">#{header[:title]}</a>]
+        if header[:subs]
+          res << "<ol>"
+          header[:subs].each do |header|
+            res << %[<li><a href="#{header[:id]}">#{header[:title]}</a>]
+          end
+          res << "</ol>"
+        end
+        res << "</li>"
       end
       res << '</ol>'
 
